096576696ec8542a7b258109027e0386916bf298
KC-25 <kierrconnell@g.crookcountyschools.org>
2022-11-11 20:55:58 -0800

    Created button and styled it.

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 console.log("Hello World.");
 
+let btn = document.createElement("button");
+btn.innerHTML = " Start Game ";
+btn.onclick = function () {
+
+    var username = prompt("Please enter the name of your detective.")
+
+};
+document.body.appendChild(btn);
