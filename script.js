9b5b10875ac013ccd3d8e7e5c0ed3b4a6215f60e
KC-25 <kierrconnell@g.crookcountyschools.org>
2022-11-14 14:15:28 -0800

    Started story.

--- script.js
+++ script.js
@@ -4,7 +4,23 @@ let btn = document.createElement("button");
 btn.innerHTML = " Start Game ";
 btn.onclick = function () {
 
-    var username = prompt("Please enter the name of your detective.")
+    // ***NAME and/or SWITCH STORIES***
+
+    if(userName != "SECRETSTORY") {
+        
+        const userName = prompt("Please enter the name of your detective.");
+        alert("Hello detective " +userName+ "!")
+    };
+
+    if(userName === "SECRETSTORY"){
+
+        alert("Switched to secret story.");
+        const UserNameMurder = prompt("Please enter the name of your murder.");
+        prompt("Please enter the name of your detective.");
+        alert("Hello detective " +UserNameMurder+ "!")
+        alert("Your objective is to kill everyone.")
+
+    };
 
 };
 document.body.appendChild(btn);
